4c721ee0778e00f225317274f96938e37bb54f76
Daiki Morita <daikimorita@DaikinoMacBook-Air.local>
2019-07-09 22:42:23 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import sys
+
+#テストを書いたらパスを記述する
+#sys.path.append('./DBM/src')
+#sys.path.append('./DBM/test')
+
+setup(
+    name="crawler",
+    version="0.1",
+    description="a crawler component for the JRA Project",
+    author="Yuma Saito and Daiki Morita",
+    url="https://github.com/dolce-song/crawler",
+    #packages=find_packages(),
+    #test_suite='DBM.discover.suite'
+
