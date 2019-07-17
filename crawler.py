93b2d9598d63c34c2e30f843dd28698d1ed47006
y-saito <zesfcyb4a7xv1v7ux9jb@gmail.com>
2019-07-17 14:23:30 +0000

    implement read robots.txt

---
+++ crawler.py
@@ -0,0 +1,24 @@
+import requests 
+import urllib.robotparser
+from bs4 import BeautifulSoup
+
+#url = "https://gihyo.jp/dp"
+#url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
+#url = "http://www.jra.go.jp/JRADB/"
+url = "http://www.jra.go.jp"
+robots_url = url + "/robots.txt"
+
+# robots.txtを読んで、クローリングの可否を判断
+rp = urllib.robotparser.RobotFileParser()
+rp.set_url(robots_url)
+rp.read()
+if (rp.can_fetch('mybot', robots_url)):
+	print("allowed to crawling " + url)
+
+# firefoxからのアクセスに偽装
+headers = {
+  "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
+}
+request = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(request.content, 'html.parser')
