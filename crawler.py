0d563993f70a322fbff56c1b0b121230d42687b6
y-saito <zesfcyb4a7xv1v7ux9jb@gmail.com>
2019-07-24 23:09:22 +0000

    modified crawler

--- crawler.py
+++ crawler.py
@@ -5,20 +5,24 @@ from bs4 import BeautifulSoup
 #url = "https://gihyo.jp/dp"
 #url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
 #url = "http://www.jra.go.jp/JRADB/"
-url = "http://www.jra.go.jp"
-robots_url = url + "/robots.txt"
+#url = "http://www.jra.go.jp"
+url = "https://race.netkeiba.com/?pid=race&id=c201903020811&mode=shutuba"
 
 # robots.txtを読んで、クローリングの可否を判断
-rp = urllib.robotparser.RobotFileParser()
-rp.set_url(robots_url)
-rp.read()
-if (rp.can_fetch('mybot', robots_url)):
-	print("allowed to crawling " + url)
+def confirmation(url):
+  robots_url = url + "/robots.txt"
+  rp = urllib.robotparser.RobotFileParser()
+  rp.set_url(robots_url)
+  rp.read()
+  if (rp.can_fetch('mybot', robots_url)):
+  	print("allowed to crawling " + url)
 
 # firefoxからのアクセスに偽装
 headers = {
   "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
 }
-request = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(request.content, 'html.parser')
+html = requests.get(url, headers=headers)
+# テキスト情報から文字コードを推定
+html.encoding = html.apparent_encoding
+soup = BeautifulSoup(html.text, 'html.parser')
+print(soup.find_all('span', attrs={'class', 'txt_smaller'}))
